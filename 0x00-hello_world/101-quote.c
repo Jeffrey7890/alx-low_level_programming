0aadb7ec5411965e0fc27bdc8d0676d49550eb9a
Jeffrey7890 <jeffrey.ogechi@gmailcom>
2022-10-13 12:48:00 -0700

    answer to advanced project

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(void)
+{
+	const char *first = "and that piece of art is use"; 
+	const char *second = "ful\" - Dora Korpar, 2015-10-19";
+	fprintf(stderr,"%s%s\n",first,second);
+	return 0;	
+}
