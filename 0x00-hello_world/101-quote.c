9c1608b82f45e781905454fa3e7e419306615cae
Jeffrey7890 <jeffrey.ogechi@gmailcom>
2022-10-13 13:12:27 -0700

    number 6

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,15 @@
 #include<stdio.h>
 
+/**
+  * main - prints strings to standard error stream
+  * Return: zero
+  */
 int main(void)
 {
-	const char *first = "and that piece of art is use"; 
+	const char *first = "and that piece of art is use";
+
 	const char *second = "ful\" - Dora Korpar, 2015-10-19";
-	fprintf(stderr,"%s%s\n",first,second);
-	return 1;	
+
+	fprintf(stderr, "%s%s\n", first, second);
+	return (1);
 }
