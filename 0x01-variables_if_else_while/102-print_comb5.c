021d4d299fe610c01dfcd0b424754d640dcc4a50
Jeffrey7890 <jeffrey.ogechi@gmailcom>
2022-10-14 11:53:53 -0700

    solution to 102-print_comb5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+  * main - pribts number combination with two digits
+  * Return: zero
+  */
+int main(void)
+{
+
+	int i, j, k, l;
+
+	for (i = 48; i < 58; i++)
+	{
+		for (j = 48; j < 58; j++)
+		{
+			for (k = 48; k < 58; k++)
+			{
+				for (l = 48; l < 58; l++)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(' ');
+					putchar(k);
+					putchar(l);
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+		putchar('\n');
+	}
+	return (0);
+}
