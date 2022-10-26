d358876b68108fa03e91aeb55a15ae913604473a
Jeffrey7890 <jeffrey.ogechi@gmailcom>
2022-10-26 01:56:53 -0700

    Commit for 7-put_helf.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,31 @@
+
+
+#include "main.h"
+
+/**
+  * puts_half - prints half a string
+  * @str: strng to half
+  */
+void puts_half(char *str)
+{
+	int length = 0, n, i;
+
+	char *start = str;
+
+	while (*start != '\0')
+	{
+		length++;
+		start++;
+	}
+
+
+	if ((length % 2) == 1)
+		n = (length - 1) / 2;
+	n = length / 2;
+
+	for (i = n; i < length; i++)
+		_putchar(*(str + i));
+
+	_putchar('\n');
+}
+
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -35,5 +35,7 @@ void _puts(char *str);
 
 void print_rev(char *s);
 
+void puts_half(char *str);
+
 void puts2(char *str);
 #endif
