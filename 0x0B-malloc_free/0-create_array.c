64826ebcf4b0b91d1162c9b1fbf699e9c5bc4c0a
Jeffrey7890 <jeffrey.ogechi@gmailcom>
2022-11-08 21:34:51 -0800

    Memory allocation 0.0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+  * create_array - creates an array of chars
+  * @size: size of array
+  * @c: initializing char
+  * Return: pointer to the array
+  */
+char *create_array(unsigned int size, char c)
+{
+	char *array;
+
+	unsigned int i = 0;
+
+	if (size == 0)
+		return (NULL);
+
+	array = malloc(sizeof(char) * size);
+
+	while (i < size)
+	{
+		array[i] = c;
+		i++;
+	}
+
+	return (array);
+}
---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,46 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <limits.h>
+
+char *create_array(unsigned int size, char c);
+int _isdigit(int c);
+
+int _isupper(int c);
+
+void print_most_numbers(void);
+
+int mul(int c, int d);
+
+void print_numbers(void);
+
+void more_numbers(void);
+
+int _putchar(char c);
+
+void print_square(int size);
+
+void print_line(int n);
+	
+void print_diagonal(int n);
+
+void rev_string(char *s);
+
+void reset_to_98(int *n);
+
+char *_strcpy(char *dest, char *src);
+
+void swap_int(int *a, int *b);
+
+int _strlen(char *s);
+
+void _puts(char *str);
+
+void print_rev(char *s);
+
+void puts_half(char *str);
+
+void print_array(int *a, int n);
+
+void puts2(char *str);
+#endif
