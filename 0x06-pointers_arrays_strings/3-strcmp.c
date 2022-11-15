053c132f8a3afdaf46ab5a6d9c51062d31e1c2fb
Jeffrey7890 <jeffrey.ogechi@gmailcom>
2022-11-15 14:54:56 -0800

    correctoin

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,3 +9,16 @@
   */
 int _strcmp(char *s1, char *s2)
 {
+	int size, i;
+
+	size = strlen(s1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (s1[i] < s2[i])
+			return (s1[i] - s2[i]);
+		else if (s1[i] > s2[i])
+			return (s1[i] - s2[i]);
+	}
+	return (0);
+}
