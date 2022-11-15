9f149ca3053f710c3a90cf499a48663e9f7aa09a
Jeffrey7890 <jeffrey.ogechi@gmailcom>
2022-11-15 13:14:17 -0800

    correction

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,23 +1,37 @@
 #include "main.h"
 #include "string.h"
+#include <stdio.h>
 
+#define OFFSET 32
 /**
-  * _isupper - checks if letter is uppercase
+  * _islower - checks if letter is lowercase
   * @c: int to check
   * Return: 1 if upper and 0 if lower
   */
-int _isupper(int c)
+int _islower(int c)
 {
-	return ((65 <= c && 90 >= c));
+	return ((97 <= c && 122 >= c));
 }
 
 /**
   * string_toupper - changes string to upper case
   * @a: stiring to change
-  * Returns: pointer to the string
+  * Return: pointer to the string
   */
 char *string_toupper(char *a)
 {
-	printf("Upper case: %c",
+	int size = 0, i;
+
+	size = strlen(a);
+
+	for (i = 0; i < size; i++)
+	{
+		if (_islower(a[i]))
+		{
+			a[i] -= OFFSET;
+		}
+
+	}
+
 	return (a);
 }
