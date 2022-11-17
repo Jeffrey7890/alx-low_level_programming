e648a4e6975986b4dcf125fcbbefd5d78e608ae2
Jeffrey7890 <jeffrey.ogechi@gmailcom>
2022-11-17 14:27:46 -0800

    -m

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,56 @@
+#include "3-calc.h"
+
+/**
+ * op_add - adds to ints
+ * @a: first int
+ * @b: second int
+ * Return: integer
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - sub to ints
+ * @a: first int
+ * @b: second int
+ * Return: integer
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - mul to ints
+ * @a: first int
+ * @b: second int
+ * Return: integer
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divides to ints
+ * @a: first int
+ * @b: second int
+ * Return: integer
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - mods to ints
+ * @a: first int
+ * @b: second int
+ * Return: integer
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
