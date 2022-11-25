ec0a16b1d32a11705c3b42ca88cb4c454c25c466
Jeffrey7890 <jeffrey.ogechi@gmailcom>
2022-11-25 10:44:53 -0800

    -m

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -2,9 +2,16 @@
 #include "string.h"
 #include "stdlib.h"
 
+/**
+ * add_node - adds a new node to list
+ * @head: head of list
+ * @str: sring of node
+ * Return: head of list
+ */
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
+
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL || str == NULL)
 	{
