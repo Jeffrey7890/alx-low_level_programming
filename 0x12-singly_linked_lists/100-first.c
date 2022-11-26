82586730d5b2afa5c0558c2c8a49f28bfda3fd75
Jeffrey7890 <jeffrey.ogechi@gmailcom>
2022-11-25 16:01:43 -0800

    Prints string before main function

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+  * print - prints string before main function
+  *
+  * Return: void
+  */
+void print(void)
+{
+	printf("You're beat! and yet, you must allow,");
+	printf("\nI bore my house upon my back!\n");
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -2,6 +2,7 @@
 #define LIST_H
 
 #include "stddef.h"
+#include "stdio.h"
 
 /**
  * struct list_s - singly linked list
@@ -18,6 +19,7 @@ typedef struct list_s
     struct list_s *next;
 } list_t;
 
+void print(void)__attribute__((constructor));
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
