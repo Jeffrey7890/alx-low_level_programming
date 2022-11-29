3126389535eb8e28283ff0cbda059ad5b7b25653
Jeffrey7890 <jeffrey.ogechi@gmailcom>
2022-11-29 05:48:24 -0800

    insert new node at index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,7 +16,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i = 0;
 
 	if (new_node == NULL)
+	{
+		free(new_node);
 		return (NULL);
+	}
 
 	if (*head == NULL)
 	{
