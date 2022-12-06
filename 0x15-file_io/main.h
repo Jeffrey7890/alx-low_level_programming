4591a509ce0559c307d7bca2bb64fab368351715
Jeffrey7890 <jeffrey.ogechi@gmailcom>
2022-12-06 13:21:34 -0800

    Appends to file end

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,8 @@
 #ifndef _MAINH_
 #define _MAINH_
 
+int append_text_to_file(const char *filename, char *text_content);
+
 int create_file(const char *filename, char *text_content);
 
 ssize_t read_textfile(const char *filename, size_t letters);
