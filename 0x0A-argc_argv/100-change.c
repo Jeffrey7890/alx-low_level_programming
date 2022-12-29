6df6ed0930236e91b1042c0ea638e57e43cd0cc0
Jeffrey7890 <jeffrey.ogechi@gmailcom>
2022-12-29 13:26:45 -0800

    get change of amount

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,67 @@
+#include "stdio.h"
+#include "stdlib.h"
+
+/**
+  * getchange - comply to betty style of < 40 per func
+  * @n: amount of money
+  * Return: change
+  */
+int getchange(int n)
+{
+	int change = 0;
+
+	while (n != 0)
+	{
+		if (n % 25 == 0)
+		{
+			n -= 25;
+			change += 1;
+		}
+		else if (n % 10 == 0)
+		{
+			n -= 10;
+			change += 1;
+		}
+		else if (n % 5 == 0)
+		{
+			n -= 5;
+			change += 1;
+		}
+		else if (n % 2 == 0)
+		{
+			n -= 2;
+			change += 1;
+		}
+		else
+		{
+			n -= 1;
+			change += 1;
+		}
+	}
+	return (change);
+}
+/**
+  * main - prints min numb of coins to make change
+  * @ac: number of args
+  * @argv: cmd line args
+  * Return: 1 if faiiled and zero on success
+  */
+int main(int ac, char **argv)
+{
+	int n, change;
+
+	if (ac == 1)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	n = atoi(argv[1]);
+	if (n < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+	change = getchange(n);
+	printf("%d\n", change);
+	return (0);
+}
