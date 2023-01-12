bb838fe0cbc13150b73039019cd94ea4fff33031
Jeffrey7890 <jeffrey.ogechi@gmailcom>
2023-01-12 15:23:44 -0800

    inserts node to indxt in list

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -47,5 +47,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		i++;
 		tmp = tmp->next;
 	}
-	return (add_dnodeint_end(h, n));
+	/* return (add_dnodeint_end(h, n)); */
+	return (NULL);
 }
