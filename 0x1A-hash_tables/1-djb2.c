21e96f297a33ed08f6ce5f73a0b09902432c6722
Jeffrey7890 <d3v3loperpy@gmail.com>
2023-02-03 06:36:15 +0100

    betty correction

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,14 +1,20 @@
 #include "hash_tables.h"
 
+/**
+ * hash_djb2 - hash function to hash a string
+ * @str: char array to hash
+ * Return: an unsigned int hashed value
+ */
 unsigned long int hash_djb2(const unsigned char *str)
 {
-    unsigned long int hash;
-    int c;
+	unsigned long int hash;
 
-    hash = 5381;
-    while ((c = *str++))
-    {
-        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-    }
-    return (hash);
+	int c;
+
+	hash = 5381;
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	}
+	return (hash);
 }
