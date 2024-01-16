2cc1e5e2648da1dde62c21938a62d188acb17ef2
Jeffrey7890 <d3v3loperpy@gmail.com>
2024-01-16 11:37:48 +0100

    add positive numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * _isdigit - checks if charbis digit
+  * @c: number to check
+  * Return: 1 if digit 0 if not
+  */
+int _isdigit(int c)
+{
+	return ((48 <= c && 58 >= c));
+}
+
+/**
+ * main - adds positive numbers from command line
+ *
+ * @argc: number of arguments
+ * @argv: argument vector
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int result, add, i;
+
+	if (argc < 2)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		add = _isdigit(*argv[i]);
+		if (add == 0)
+		{
+			printf("Error\n");
+			return (0);
+		}
+		result += atoi(argv[i]);
+	}
+	printf("%d\n", result);
+	return (0);
+}
