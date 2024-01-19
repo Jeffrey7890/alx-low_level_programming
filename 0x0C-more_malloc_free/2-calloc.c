d7ddcdd42e9044d226d8dd9887d50d650425ae92
Jeffrey7890 <d3v3loperpy@gmail.com>
2024-01-19 23:37:43 +0100

    create mem for array

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdlib.h>
+#include <limits.h>
+
+
+/**
+ * _calloc - allocates array memory using malloc
+ *
+ * @nmemb: number of member in array
+ * @size: size of memory
+ *
+ * Return: pointer to memory or NULL if fail
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned char *result;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	if (nmemb >= UINT_MAX / size)
+		return (NULL);
+
+	result = malloc(nmemb * size);
+
+	if (result == NULL)
+	{
+		free(result);
+		return (NULL);
+	}
+
+	i = 0;
+	while (i < nmemb)
+	{
+		result[i] = 0;
+		i++;
+	}
+
+	return (result);
+}
