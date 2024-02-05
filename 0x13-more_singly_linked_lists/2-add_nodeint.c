03126accc81a6d269b87d6062fc1f415aed9c9ea
Jeffrey7890 <jeffrey.ogechi@gmail.com>
2024-02-05 19:48:14 +0100

    add mew node to list

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdlib.h>
+
+
+/**
+ * add_nodeint - add new element to the list
+ * @head: head of the list
+ * @n: new number
+ * Return: pointer to new element
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new;
+
+	new = malloc(sizeof(listint_t));
+
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->next = *head;
+	*head = new;
+	return (*head);	
+
+}
