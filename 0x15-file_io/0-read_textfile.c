f06e17fb67627815e04cbf239b9fc146d9783cb2
Jeffrey7890 <jeffrey.ogechi@gmail.com>
2024-02-16 21:19:54 +0100

    free mem

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -19,15 +19,16 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (filename == NULL)
 		return (0);
+
+
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+		return (0);
+
 	buf = malloc(sizeof(char) * (letters + 1));
 
 	if (buf == NULL)
 		return (0);
-
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
-		return (0);
-
 	nread = read(fd, buf, letters);
 	buf[nread] = '\0';
 	if (nread <= 0)
