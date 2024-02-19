b8ac06ab34d4d579c1d1de6e4c1ac31d4aad8c46
Jeffrey7890 <d3v3loperpy@gmail.com>
2024-02-19 01:18:13 +0100

    create new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -21,10 +21,25 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 
 	if (name != NULL)
+	{
 		d->name = strdup(name);
+		if (d->name == NULL)
+		{
+			free(d);
+			return (NULL);
+		}
+	}
 	d->age = age;
 	if (owner != NULL)
+	{
 		d->owner = strdup(owner);
+		if (d->owner == NULL)
+		{
+			free(d->name);
+			free(d);
+			return (NULL);
+		}
+	}
 
 	return (d);
 }
