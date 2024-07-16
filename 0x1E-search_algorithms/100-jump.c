7cc39c0193d20e24a168b46e06d426903b1bc875
jeffrey7890 <d3v3loperpy@gmail.com>
2024-07-16 05:35:29 -0700

    must obey betty

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -10,7 +10,7 @@
  */
 size_t min(size_t a, size_t b)
 {
-	return (a < b ? a: b);
+	return (a < b ? a : b);
 }
 
 /**
@@ -22,52 +22,42 @@ size_t min(size_t a, size_t b)
  */
 int jump_search(int *array, size_t n, int value)
 {
-	size_t a, b,s;
-	size_t x = 0,y = 0;
+	size_t a, b, s;
+	size_t x = 0, y = 0;
 
 	a = 0;
 	s = sqrt(n);
 	b = s;
-
-
-
 	while (array[min(b, n) - 1] < value)
 	{
-	
 		printf("Value checked array[%ld] = [%d]\n", a, array[a]);
-		x=a;
+		x = a;
 		a = b;
-		y=a;
+		y = a;
 		if (a >= n)
 		{
 			break;
 		}
 		b += s;
 	}
-
 	if (x == 0 && y == 0)
 	{
 		printf("Value checked array[%ld] = [%d]\n", x, array[x]);
 		x = a;
 		y = b;
 	}
-
 	printf("Value found between indexes [%ld] and [%ld]\n", x, y);
 	while (array[x] < value && x < n)
 	{
 		printf("Value checked array[%ld] = [%d]\n", x, array[x]);
 		x += 1;
-
 		if (x == min(b, n))
 			return (-1);
 	}
-
 	if (array[x] == value)
 	{
 		printf("Value checked array[%ld] = [%d]\n", x, array[x]);
-
 		return (x);
 	}
-	printf("hdhdhshs");
 	return (-1);
 }
